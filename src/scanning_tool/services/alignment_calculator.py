c9ab5e1bd89a7f2ce23e1f0542f1b909618eba85
Romans Pokrovskis <motivated.it@gmail.com>
2026-04-19 22:59:18 +0300

    refactor: move alignment position calculation to AlignmentCalculator class

---
+++ src/scanning_tool/services/alignment_calculator.py
@@ -0,0 +1,24 @@
+from scanning_tool.domain.alignment import AnchorDetection, CaptureRegion
+from scanning_tool.domain.common import Offset2D
+
+
+class AlignmentCalculator:
+    @staticmethod
+    def calculate_aligned_position(
+        detection: AnchorDetection,
+        capture_region: CaptureRegion,
+        anchor_offset: Offset2D,
+    ) -> tuple[int, int]:
+        base_left = (
+            detection.match_left
+            + (detection.template_width / 2.0)
+            - (capture_region.width / 2.0)
+        )
+        base_top = (
+            detection.match_top
+            + (detection.template_height / 2.0)
+            - (capture_region.height / 2.0)
+        )
+        new_left = int(round(base_left + anchor_offset.x))
+        new_top = int(round(base_top + anchor_offset.y))
+        return new_left, new_top
--- src/scanning_tool/services/alignment_service.py
+++ src/scanning_tool/services/alignment_service.py
@@ -1,6 +1,7 @@
 """Auto-alignment background service."""
 
 from typing import Callable, Optional, Tuple
+from scanning_tool.services.alignment_calculator import AlignmentCalculator
 from scanning_tool.services.base_service import BaseService
 from scanning_tool.core.anchor import AnchorRegionTracker
 from scanning_tool.domain.alignment import (
@@ -12,28 +13,6 @@ from scanning_tool.domain.alignment import (
 from scanning_tool.domain.common import Offset2D
 
 SyncCallback = Callable[[], None]
-
-
-class AlignmentCalculator:
-    @staticmethod
-    def calculate_aligned_position(
-        detection: AnchorDetection,
-        capture_region: CaptureRegion,
-        anchor_offset: Offset2D,
-    ) -> tuple[int, int]:
-        base_left = (
-            detection.match_left
-            + (detection.template_width / 2.0)
-            - (capture_region.width / 2.0)
-        )
-        base_top = (
-            detection.match_top
-            + (detection.template_height / 2.0)
-            - (capture_region.height / 2.0)
-        )
-        new_left = int(round(base_left + anchor_offset.x))
-        new_top = int(round(base_top + anchor_offset.y))
-        return new_left, new_top
 
 
 def reset_alignment_info(info: AlignmentInfo) -> None:
